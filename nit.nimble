cdbceb2c5194fa1a7b4099a822265bb996fea04a
Hasan Majid <hasanmajid3.14@hotmail.com>
2023-08-12 13:51:00 -0400

    initial commit

---
+++ nit.nimble
@@ -0,0 +1,13 @@
+# Package
+
+version       = "0.1.0"
+author        = "Hasan Majid"
+description   = "A new nimble version control system!"
+license       = "MIT"
+srcDir        = "src"
+bin           = @["nit"]
+
+
+# Dependencies
+
+requires "nim >= 2.0.0"
---
+++ src/nit.nim
@@ -0,0 +1,83 @@
+# This is just an example to get you started. A typical binary package
+# import iterutils
+
+# import sequtils
+import strutils
+import os
+
+proc diff*(fileName1: string, fileName2: string) =
+
+  var file1: string
+  var file2: string
+
+  try:
+    file1 = readFile(fileName1)
+  except IOError:
+    echo "Error! Cannot read file: ", fileName1
+    return
+
+  try:
+    file2 = readFile(fileName2)
+  except IOError:
+    echo "Error! Cannot read file: ", fileName2
+    return
+
+  # echo file[file.len-1]
+
+  let lines1: seq[string] = file1.split('\n') ## Splits file string into a seq via newline character \n
+  let lines2: seq[string] = file2.split('\n') ## Splits file string into a seq via newline character \n
+
+  echo lines1
+  echo lines2 
+  var numLines = max(lines1.len, lines2.len) ## Finds the file with the longest number of lines
+  echo()  
+
+  ## 
+  for i in 0..numLines-1:
+    var x = lines1[i]
+    var y = lines2[i]
+    x.removeSuffix('\r')
+    y.removeSuffix('\r')
+
+    if x == y:
+      echo "\"", $i, ". ", x, "\"\n"
+    if x != y:
+      echo "DIFF:"
+      echo "\"", $i, ". ", x, "\"\n"
+      echo "\"", $i, ". ", y, "\"\n"
+    echo()
+
+when isMainModule:
+  echo()
+  if paramCount() == 3:
+    if paramStr(1) == "diff":
+      diff(paramStr(2), paramStr(3))
+
+
+
+
+
+
+  ## can't loop through 3 seqs at once, only 2
+  # for (x, y, z) in items(zip(lines1, lines2, lineNums)):
+  #   if x == y:
+  #     echo x, y
+  #   if x != y:
+  #     echo "DIFF:\n", x, "\n", y
+  #   echo()
+  
+  # var out1 = ""
+  # var out2 = ""
+  # for (x, y) in zip(file, file2):
+  #   if x != y:
+  #     out1 = out1 & x
+  #     out2 = out2 & y
+
+  # echo out1
+  # echo out2
+
+
+
+      
+
+
